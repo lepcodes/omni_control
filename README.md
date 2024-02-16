8512bb48731cea76ea1913bdd9e524dc4e60a144
Luispre99 <luis.preciado3077@alumnos.udg.mx>
2024-02-16 14:02:04 -0700

    Arduino code modified and four motors working

--- README.md
+++ README.md
@@ -8,3 +8,23 @@ To run this package use
     git clone https://github.com/Luispre99/omni_control.git
     cd ..
     catkin_make
+
+## Development
+
+To modify the Arduino code or if you change the structure of the custom message, you have to install Arduino and the rosserial library
+
+Besides, you need to have this ros-packages
+
+    sudo apt-get install ros-melodic-rosserial-python
+    sudo apt-get install ros-melodic-rosserial-arduino
+    sudo apt-get install ros-melodic-rosserial
+
+Then run
+    
+    cd ~/omni_ros_ws/src/omni_control
+    rosrun rosserial_client make_libraries ~/Arduino/libraries omni_control
+    sudo cp -R ~/Arduino/libraries/ros_lib/omni_control ~/Arduino/libraries/Rosserial_Arduino_Library/src
+
+To generate and move the header files from custom messages in this case OmniVel.h
+
+Finally, install the SaberTooth libraries from [here]()
--- omni_ST/omni_ST.ino
+++ omni_ST/omni_ST.ino
@@ -10,6 +10,7 @@ SoftwareSerial SWSerial2(NOT_A_PIN, 5); // RX on no pin (unused), TX on pin 5 (t
 ros::NodeHandle  nh;
 
 void Velocidadmsg( const geometry_msgs::Quaternion& omni_vel){
+  digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
   int wheel_1, wheel_2, wheel_3, wheel_4;
   wheel_1 = map(omni_vel.x,-10,10,128,255);
   wheel_2 = map(omni_vel.w,-10,10,1  ,127);
@@ -23,10 +24,15 @@ void Velocidadmsg( const geometry_msgs::Quaternion& omni_vel){
   SWSerial1.write(wheel_1); //Motor 1
   SWSerial2.write(wheel_3); //Motor 3
 
+  digitalWrite(1,LOW); //S2_1
+  digitalWrite(2,LOW); //S2_2
+  delay(1);
+  digitalWrite(1,HIGH); //S2_1
+  digitalWrite(2,HIGH); //S2_2
+  
   //SET Motor 2 and Motor 4
   SWSerial1.write(wheel_2); //Motor 2
   SWSerial2.write(wheel_4); //Motor 4
-
   digitalWrite(1,LOW); //S2_1
   digitalWrite(2,LOW); //S2_2
 }
@@ -37,6 +43,8 @@ void setup()
 {
   SWSerial1.begin(38400);
   SWSerial2.begin(38400);
+  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
   SabertoothTXPinSerial.begin(38400);
   pinMode(1,OUTPUT); //S2_1: when it's high ejecute the information and when it's low don't do anything.
   pinMode(2,OUTPUT); //S2_2: when it's high ejecute the information and when it's low don't do anything.
