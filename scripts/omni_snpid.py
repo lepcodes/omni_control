02ffbf679c33dff2a9f72734550ba2398ac296dd
Luispre99 <luis.preciado3077@alumnos.udg.mx>
2024-04-30 15:56:55 -0700

    Defining transformation matrices for the pose and calibration procedure

--- scripts/omni_snpid.py
+++ scripts/omni_snpid.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
+import tf
 import time
 import math
 import rospy 
 import threading
 import numpy as np
 from turtlesim.msg import Pose
-from tf.transformations import euler_from_quaternion
+from tf.transformations import euler_from_quaternion, euler_from_matrix
 from geometry_msgs.msg import Quaternion, PoseStamped
 from sensor_msgs.msg import Joy
 
@@ -20,6 +21,7 @@ class ControlNode:
 
         self.joyIsWorking = 0
         self.calibInProgress = 1
+        self.transformer = tf.TransformerROS()
 
         # Initializing Pose and Velocities
 
@@ -62,133 +64,131 @@ class ControlNode:
         self.beta = 0.01
 
     def control(self):
-        if self.calibInProgress == 1:
-            
-            # Error Computation
-
-            self.e_p = self.pd_list[self.i] - self.p
-            self.e_i = self.e_i + self.e_prime
-            self.e_d = self.e_p - self.e_prev
-            self.e_prev = self.e_p
-
-            # Define SNPIDs Inputs
-
-            X_1 = np.matrix([float(self.e_p[0]), float(self.e_i[0]), float(self.e_d[0])]).T
-            X_2 = np.matrix([float(self.e_p[1]), float(self.e_i[1]), float(self.e_d[1])]).T
-            X_3 = np.matrix([float(self.e_p[2]), float(self.e_i[2]), float(self.e_d[2])]).T
-
-            # Check if Joystick is Sending Data
-
-            if self.joyIsWorking == 0:
-            
-                # Compute Weighted Sums of SNPIDs
-
-                V_1 = self.W_1.T*X_1
-                V_2 = self.W_2.T*X_2
-                V_3 = self.W_3.T*X_3
-
-                # Compute SNPIDs Outputs
-
-                U_1 = self.alpha_1*np.tanh(V_1)
-                U_2 = self.alpha_2*np.tanh(V_2)
-                U_3 = self.alpha_3*np.tanh(V_3)
-                
-                # U_1 = float(V_1)
-                # U_2 = float(V_2)
-                # U_3 = float(V_3)
-
-                # Saturate Output (If Necessary)
-
-                U_1 = float(np.clip(U_1,-.2,.2))
-                U_2 = float(np.clip(U_2,-.2,.2))
-                U_3 = float(np.clip(U_3,-.2,.2))
-
-                # Robot Inverse Kinematics (Control Velocities)
-
-                pi = np.pi
-                alpha = self.p[2] + pi/4
-                L = 0.425/2
-                l = 0.44/2
-                T = np.array([[np.sqrt(2)*np.sin(alpha), -np.sqrt(2)*np.cos(alpha), -(L+l)],
-                            [np.sqrt(2)*np.cos(alpha),  np.sqrt(2)*np.sin(alpha),  (L+l)],
-                            [np.sqrt(2)*np.cos(alpha),  np.sqrt(2)*np.sin(alpha), -(L+l)],
-                            [np.sqrt(2)*np.sin(alpha), -np.sqrt(2)*np.cos(alpha),  (L+l)]])
-                v = np.array([[U_1],[U_2],[U_3]])
-                self.u = np.dot(T, v)/0.05
-
-                # Publish Wheel Velocities
-
-                self.publish_velocity()
-
-                # Display SNPIDs Data
-
-                print(" ")
-                print("{:<10}|{:<10}|{:<10}|{:<10}|{:<10}|{:<10}|{:<10}|{:<10}|{:<10}".format("Pose","Pose_d","Errors (x)","Errors (y)","Errors (t)","Gains (x)","Gains (y)","Gains (t)","Control"))
-                for i in range(3):
-                    print("{:<10.4f}|{:<10.4f}|{:<10.4f}|{:<10.4f}|{:<10.4f}|{:<10.4f}|{:<10.4f}|{:<10.4f}|{:<10.4f}".format(self.p[i],self.pd_list[self.i][i],X_1[i,0],X_2[i,0],X_3[i,0],self.W_1[i,0],self.W_2[i,0],self.W_3[i,0],v[i,0]))
-                
-                # EKF Training Algorithm
-                    
-                H_1 = self.alpha_1*(1-float(np.tanh(V_1)**2))*X_1
-                H_2 = self.alpha_2*(1-float(np.tanh(V_2)**2))*X_2
-                H_3 = self.alpha_3*(1-float(np.tanh(V_3)**2))*X_3
-
-                # H_1 = X_1
-                # H_2 = X_2
-                # H_3 = X_3
-
-                K_1 = self.P_1*H_1*np.linalg.inv(self.R_1 + H_1.T*self.P_1*H_1)
-                K_2 = self.P_2*H_2*np.linalg.inv(self.R_2 + H_2.T*self.P_2*H_2)
-                K_3 = self.P_3*H_3*np.linalg.inv(self.R_3 + H_3.T*self.P_3*H_3)
-
-                self.W_1 = self.W_1 + self.eta_1*(K_1*self.e_p[0] - self.beta*float(self.e_p[0])*self.W_1)
-                self.W_2 = self.W_2 + self.eta_2*(K_2*self.e_p[1] - self.beta*float(self.e_p[1])*self.W_2)
-                self.W_3 = self.W_3 + self.eta_3*(K_3*self.e_p[2] - self.beta*float(self.e_p[2])*self.W_3)
-
-                self.W_1 = abs(self.W_1)
-                self.W_2 = abs(self.W_2)
-                self.W_3 = abs(self.W_3)
-
-                self.P_1 = self.P_1 - K_1*H_1.T*self.P_1 + self.Q_1
-                self.P_2 = self.P_2 - K_2*H_2.T*self.P_2 + self.Q_2
-                self.P_3 = self.P_3 - K_3*H_3.T*self.P_3 + self.Q_3
-
-                # Anti-WindUp
-
-                if abs(float(V_1)) > self.alpha_1:
-                    prime_1 = 0
-                else:
-                    prime_1 = self.e_p[0]
-                if abs(float(V_2)) > self.alpha_2:
-                    prime_2 = 0
-                else:
-                    prime_2 = self.e_p[1]
-                if abs(float(V_3)) > self.alpha_3:
-                    prime_3 = 0
-                else:
-                    prime_3 = self.e_p[2]
-
-                self.e_prime = np.array([prime_1,prime_2,prime_3])
-
-                print(self.e_prime)
-
-            else:
-                print('JoyStick is Working!')
-
-                # Display SNPIDs Data
-
-                print(" ")
-                print("{:<10}|{:<10}|{:<10}|{:<10}|{:<10}|{:<10}|{:<10}|{:<10}".format("Pose","Pose_d","Errors (x)","Errors (y)","Errors (tht)","Gains (x)","Gains (y)","Gains (tht)"))
-                for i in range(3):
-                    print("{:<10.4f}|{:<10.4f}|{:<10.4f}|{:<10.4f}|{:<10.4f}|{:<10.4f}|{:<10.4f}|{:<10.4f}".format(self.p[i],self.pd_list[self.i][i],X_1[i,0],X_2[i,0],X_3[i,0],self.W_1[i,0],self.W_2[i,0],self.W_3[i,0]))
-            
-    def calibration(self):
         
-        if self.calibInProgress == 1:
-            rospy.loginfo("Move the Robot to the front 1 meter")
-            raw_input("Hit Enter when the robot is in place...")
-            x = self.p[0]
+        # Error Computation
+
+        self.e_p = self.pd_list[self.i] - self.p
+        self.e_i = self.e_i + self.e_prime
+        self.e_d = self.e_p - self.e_prev
+        self.e_prev = self.e_p
+
+        # Define SNPIDs Inputs
+
+        X_1 = np.matrix([float(self.e_p[0]), float(self.e_i[0]), float(self.e_d[0])]).T
+        X_2 = np.matrix([float(self.e_p[1]), float(self.e_i[1]), float(self.e_d[1])]).T
+        X_3 = np.matrix([float(self.e_p[2]), float(self.e_i[2]), float(self.e_d[2])]).T
+
+        # Check if Joystick is Sending Data
+
+        if self.joyIsWorking == 0:
+        
+            # Compute Weighted Sums of SNPIDs
+
+            V_1 = self.W_1.T*X_1
+            V_2 = self.W_2.T*X_2
+            V_3 = self.W_3.T*X_3
+
+            # Compute SNPIDs Outputs
+
+            U_1 = self.alpha_1*np.tanh(V_1)
+            U_2 = self.alpha_2*np.tanh(V_2)
+            U_3 = self.alpha_3*np.tanh(V_3)
             
+            # U_1 = float(V_1)
+            # U_2 = float(V_2)
+            # U_3 = float(V_3)
+
+            # Saturate Output (If Necessary)
+
+            U_1 = float(np.clip(U_1,-.2,.2))
+            U_2 = float(np.clip(U_2,-.2,.2))
+            U_3 = float(np.clip(U_3,-.2,.2))
+
+            # Robot Inverse Kinematics (Control Velocities)
+
+            pi = np.pi
+            alpha = self.p[2] + pi/4
+            L = 0.425/2
+            l = 0.44/2
+            T = np.array([[np.sqrt(2)*np.sin(alpha), -np.sqrt(2)*np.cos(alpha), -(L+l)],
+                        [np.sqrt(2)*np.cos(alpha),  np.sqrt(2)*np.sin(alpha),  (L+l)],
+                        [np.sqrt(2)*np.cos(alpha),  np.sqrt(2)*np.sin(alpha), -(L+l)],
+                        [np.sqrt(2)*np.sin(alpha), -np.sqrt(2)*np.cos(alpha),  (L+l)]])
+            v = np.array([[U_1],[U_2],[U_3]])
+            self.u = np.dot(T, v)/0.05
+
+            # Publish Wheel Velocities
+
+            self.publish_velocity()
+
+            # Display SNPIDs Data
+
+            print(" ")
+            print("{:<10}|{:<10}|{:<10}|{:<10}|{:<10}|{:<10}|{:<10}|{:<10}|{:<10}".format("Pose","Pose_d","Errors (x)","Errors (y)","Errors (t)","Gains (x)","Gains (y)","Gains (t)","Control"))
+            for i in range(3):
+                print("{:<10.4f}|{:<10.4f}|{:<10.4f}|{:<10.4f}|{:<10.4f}|{:<10.4f}|{:<10.4f}|{:<10.4f}|{:<10.4f}".format(self.p[i],self.pd_list[self.i][i],X_1[i,0],X_2[i,0],X_3[i,0],self.W_1[i,0],self.W_2[i,0],self.W_3[i,0],v[i,0]))
+            print(self.calFactor)
+            # EKF Training Algorithm
+                
+            H_1 = self.alpha_1*(1-float(np.tanh(V_1)**2))*X_1
+            H_2 = self.alpha_2*(1-float(np.tanh(V_2)**2))*X_2
+            H_3 = self.alpha_3*(1-float(np.tanh(V_3)**2))*X_3
+
+            # H_1 = X_1
+            # H_2 = X_2
+            # H_3 = X_3
+
+            K_1 = self.P_1*H_1*np.linalg.inv(self.R_1 + H_1.T*self.P_1*H_1)
+            K_2 = self.P_2*H_2*np.linalg.inv(self.R_2 + H_2.T*self.P_2*H_2)
+            K_3 = self.P_3*H_3*np.linalg.inv(self.R_3 + H_3.T*self.P_3*H_3)
+
+            self.W_1 = self.W_1 + self.eta_1*(K_1*self.e_p[0] - self.beta*float(self.e_p[0])*self.W_1)
+            self.W_2 = self.W_2 + self.eta_2*(K_2*self.e_p[1] - self.beta*float(self.e_p[1])*self.W_2)
+            self.W_3 = self.W_3 + self.eta_3*(K_3*self.e_p[2] - self.beta*float(self.e_p[2])*self.W_3)
+
+            self.W_1 = abs(self.W_1)
+            self.W_2 = abs(self.W_2)
+            self.W_3 = abs(self.W_3)
+
+            self.P_1 = self.P_1 - K_1*H_1.T*self.P_1 + self.Q_1
+            self.P_2 = self.P_2 - K_2*H_2.T*self.P_2 + self.Q_2
+            self.P_3 = self.P_3 - K_3*H_3.T*self.P_3 + self.Q_3
+
+            # Anti-WindUp
+
+            if abs(float(V_1)) > self.alpha_1:
+                prime_1 = 0
+            else:
+                prime_1 = self.e_p[0]
+            if abs(float(V_2)) > self.alpha_2:
+                prime_2 = 0
+            else:
+                prime_2 = self.e_p[1]
+            if abs(float(V_3)) > self.alpha_3:
+                prime_3 = 0
+            else:
+                prime_3 = self.e_p[2]
+
+            self.e_prime = np.array([prime_1,prime_2,prime_3])
+
+            print(self.e_prime)
+
+        else:
+            print('JoyStick is Working!')
+
+            # Display SNPIDs Data
+
+            print(" ")
+            print("{:<10}|{:<10}|{:<10}|{:<10}|{:<10}|{:<10}|{:<10}|{:<10}".format("Pose","Pose_d","Errors (x)","Errors (y)","Errors (tht)","Gains (x)","Gains (y)","Gains (tht)"))
+            for i in range(3):
+                print("{:<10.4f}|{:<10.4f}|{:<10.4f}|{:<10.4f}|{:<10.4f}|{:<10.4f}|{:<10.4f}|{:<10.4f}".format(self.p[i],self.pd_list[self.i][i],X_1[i,0],X_2[i,0],X_3[i,0],self.W_1[i,0],self.W_2[i,0],self.W_3[i,0]))
+        
+    def calibration(self):
+
+        rospy.loginfo("Move the Robot 1 meter in 'x' axis")
+        raw_input("Hit Enter when the robot is in place...")
+        x = self.p[0]
+        self.calFactor = 1/x
 
     def callback_pose(self, data):
 
@@ -202,6 +202,20 @@ class ControlNode:
         (_,_,yaw) = euler_from_quaternion(q)
 
         self.p = np.array([data.pose.position.x,data.pose.position.y,yaw]) 
+
+        # Transformation Matrix
+
+        gTc = self.transformer.fromTranslationRotation((data.pose.position.x,data.pose.position.y,data.pose.position.z),q)
+        cTo = np.array([[0, 1, 0, 1],
+                        [0, 0, 1, 1],
+                        [1, 0, 1, 1],
+                        [0, 0, 0, 1]]).astype(float)
+        gTo = np.dot(gTc,cTo)
+
+        # Calibrated Pose
+        
+        # self.p = np.arra([self.calFactor*data.pose.position.x,self.calFactor*data.pose.position.y,yaw])
+        (_,_,yaw) = euler_from_quaternion(q)
 
     def callback_joy(self, data):
 
@@ -233,7 +247,7 @@ if __name__ == '__main__':
     
     rospy.init_node('Control_SNPID', anonymous=True)
     control = ControlNode()
-
+    control.calibration()
     rate = rospy.Rate(10)  # Adjust the rate as needed (e.g., 10 Hz)
     while not rospy.is_shutdown():
         control.control() 
