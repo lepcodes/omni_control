ced0a4de58fa876d42474268e4ae53df4f1b1639
Luispre99 <luis.preciado3077@alumnos.udg.mx>
2024-08-08 16:30:54 -0600

    Last changes made to the repository

--- scripts/omni_snpid.py
+++ scripts/omni_snpid.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
 import tf
+import csv
 import time
 import math
 import rospy 
+import pickle
+import rospkg
 import threading
 import numpy as np
+import tf.transformations as tf_trans
 from turtlesim.msg import Pose
 from tf.transformations import euler_from_quaternion, euler_from_matrix
 from geometry_msgs.msg import Quaternion, PoseStamped
@@ -13,12 +17,18 @@ from sensor_msgs.msg import Joy
 class ControlNode:
     def __init__(self):
         self.publisher = rospy.Publisher('omni_vel', Quaternion, queue_size=10)
-        self.subscriber= rospy.Subscriber('posesim/pose', PoseStamped, self.callback_pose)
+        self.subscriber= rospy.Subscriber('posestamped', PoseStamped, self.callback_pose)
         self.subscriber= rospy.Subscriber('joy', Joy, self.callback_joy)
-
-        self.pd_list = [np.array([1, 0, 0]), np.array([1, 1, 0]), np.array([0, 1, 0]), np.array([0, 0, 0])]
+        
+        self.loadPose = True
+        self.start_time = time.time()
+        self.prevTime = time.time()
+        self.pd_list, self.quaternion_list = self.load_pose_list()  
+        self.pose_list = []
+        self.posed_list = []
         self.i = 0
-
+        rospy.on_shutdown(self.save_pose_list)
+        
         self.joyIsWorking = 0
         self.joyScaleX = 0.2
         self.joyScaleY = 0.2
@@ -26,10 +36,11 @@ class ControlNode:
         self.calibInProgress = 1
         self.transformer = tf.TransformerROS()
 
-        # Initializing Pose and Velocities
-
+        # Initializing Pose
+        
+        self.quaternion = np.array([0, 0, 0, 0])
         self.p = np.array([0, 0, 0])
-        self.pp = np.array([0, 0, 0])
+        self.pd = np.array([0, 0, 0])
 
         # Initializing Errors
 
@@ -66,11 +77,74 @@ class ControlNode:
         self.eta_3 = 0.02
         self.beta = 0.01
 
+    def load_pose_list(self):
+        rospack = rospkg.RosPack()
+        path = rospack.get_path('omni_control')
+        file_path = path + '/pose_list.csv'
+        pose_list = []
+        
+        with open(file_path, 'r') as f:
+            reader = csv.reader(f)
+            for row in reader:
+                pose = np.array([float(element) for element in row])
+                pose_list.append(pose)
+       
+        pickle_file_path = path + '/np_array.pkl'
+        with open(pickle_file_path, 'rb') as f:
+            quaternion_list = pickle.load(f)
+
+        return pose_list,quaternion_list
+    
     def control(self):
         
-        # Error Computation
+        # Desired Pose Computation
+        
+        if self.loadPose == False:
+            t = time.time() - self.start_time
+            r = 1
+            omega = 2 * np.pi / 200
+            phi = 0
+            x = r * np.cos(omega * t + phi)
+            y = r * np.sin(omega * t + phi)
+            self.pd = np.array([x, y, 0])
+        else:
+            self.pd  = self.pd_list[self.i]
+            
+        # Error Computation   
+        
+        self.e_p = self.pd - self.p #Proportional Error for x, y
 
-        self.e_p = self.pd_list[self.i] - self.p
+        # Quaternion Error Computation
+
+        q_current_conj = tf_trans.quaternion_conjugate(self.quaternion)
+        q_error = tf_trans.quaternion_multiply(self.quaternion_list[self.i], q_current_conj)
+        theta_error = 2*math.atan2(np.linalg.norm(q_error[:2]), q_error[3])
+        
+        rotation_matrix = tf_trans.quaternion_matrix(q_error)[:3, :3]
+
+        # Ensure the trace of the rotation matrix is within the valid range [-1, 3]
+        trace = np.clip((np.trace(rotation_matrix) - 1) / 2, -1, 1)
+
+        # Calculate the angle of rotation
+        angle = np.arccos(trace)
+
+        # Calculate the axis of rotation
+        axis = np.array([
+            rotation_matrix[2, 1] - rotation_matrix[1, 2],
+            rotation_matrix[0, 2] - rotation_matrix[2, 0],
+            rotation_matrix[1, 0] - rotation_matrix[0, 1]
+        ])
+        if np.linalg.norm(axis) != 0:
+            axis = axis / np.linalg.norm(axis)
+            
+        if axis[1] > 0:
+            # Axis points in the positive Y direction, the error is positive
+            angle = -angle
+        else:
+            # Axis points in the negative Y direction, the error is negative
+            angle = angle
+        self.e_p[2] = angle # Proportional error of theta based on quaternion
+        
         self.e_i = self.e_i + self.e_prime
         self.e_d = self.e_p - self.e_prev
         self.e_prev = self.e_p
@@ -120,12 +194,10 @@ class ControlNode:
             # Display SNPIDs Data
             
             v = [self.v_x,self.v_y,self.v_theta]
-            print(v)
             print(" ")
             print("{:<10}|{:<10}|{:<10}|{:<10}|{:<10}|{:<10}|{:<10}|{:<10}|{:<10}".format("Pose","Pose_d","Errors (x)","Errors (y)","Errors (t)","Gains (x)","Gains (y)","Gains (t)","Control"))
             for i in range(3):
-                print("{:<10.4f}|{:<10.4f}|{:<10.4f}|{:<10.4f}|{:<10.4f}|{:<10.4f}|{:<10.4f}|{:<10.4f}|{:<10.4f}".format(self.p[i],self.pd_list[self.i][i],X_1[i,0],X_2[i,0],X_3[i,0],self.W_1[i,0],self.W_2[i,0],self.W_3[i,0],v[i]))
-            print(self.calFactor)
+                print("{:<10.4f}|{:<10.4f}|{:<10.4f}|{:<10.4f}|{:<10.4f}|{:<10.4f}|{:<10.4f}|{:<10.4f}|{:<10.4f}".format(self.p[i],self.pd[i],X_1[i,0],X_2[i,0],X_3[i,0],self.W_1[i,0],self.W_2[i,0],self.W_3[i,0],v[i]))
             
             # EKF Training Algorithm
                 
@@ -170,7 +242,6 @@ class ControlNode:
 
             self.e_prime = np.array([prime_1,prime_2,prime_3])
 
-            print(self.e_prime)
 
         else:
 
@@ -181,40 +252,26 @@ class ControlNode:
             for i in range(3):
                 print("{:<10.4f}|{:<10.4f}|{:<10.4f}|{:<10.4f}|{:<10.4f}|{:<10.4f}|{:<10.4f}|{:<10.4f}".format(self.p[i],self.pd_list[self.i][i],X_1[i,0],X_2[i,0],X_3[i,0],self.W_1[i,0],self.W_2[i,0],self.W_3[i,0]))
         
-    def calibration(self):
-
-        rospy.loginfo("Move the Robot 1 meter in 'x' axis")
-        raw_input("Hit Enter when the robot is in place...")
-        x = self.p[0]
-        self.calFactor = 1/x
-
     def callback_pose(self, data):
 
-        # Simulated Pose
-
-        q = (data.pose.orientation.x,
-             data.pose.orientation.y,
-             data.pose.orientation.z,
-             data.pose.orientation.w)
+        # Get Pose from Message
         
-        (_,_,yaw) = euler_from_quaternion(q)
-
-        self.p = np.array([data.pose.position.x,data.pose.position.y,yaw]) 
-
-        # Transformation Matrix
-
-        gTc = self.transformer.fromTranslationRotation((data.pose.position.x,data.pose.position.y,data.pose.position.z),q)
-        cTo = np.array([[0, 1, 0, 1],
-                        [0, 0, 1, 1],
-                        [1, 0, 1, 1],
-                        [0, 0, 0, 1]]).astype(float)
-        gTo = np.dot(gTc,cTo)
-
-        # Calibrated Pose
+        pose = (data.pose.position.x,
+                data.pose.position.y,
+                data.pose.position.z)
+        quaternion = (data.pose.orientation.x,
+                      data.pose.orientation.y,
+                      data.pose.orientation.z,
+                      data.pose.orientation.w)
         
-        # self.p = np.arra([self.calFactor*data.pose.position.x,self.calFactor*data.pose.position.y,yaw])
-        (_,_,yaw) = euler_from_quaternion(q)
-
+        # Set Pose of Center of Omni-Robot
+        
+        (_,pitch,_) = euler_from_quaternion(quaternion)
+        self.p = np.array([data.pose.position.z,-data.pose.position.x, -pitch])
+        self.quaternion = np.array(quaternion)
+        
+        self.pose_list.append(self.p)
+        self.posed_list.append(self.pd)
     def callback_joy(self, data):
         
         # Get Enable Button Status (11)
@@ -236,8 +293,8 @@ class ControlNode:
     def publish_velocity(self):
         # Robot Orientation
 
-        #theta = self.p[2]
-        theta = 0
+        theta = self.p[2]
+        #theta = 0
 
         # Robot Inverse Kinematics
 
@@ -251,7 +308,6 @@ class ControlNode:
                       [np.sqrt(2)*np.sin(alpha), -np.sqrt(2)*np.cos(alpha),  (L+l)]])
         
         v = np.array([self.v_x, self.v_y, self.v_theta])
-        #print(v)
         self.u = np.dot(T,v)/0.05
         
         #Publishing Wheel Velocities
@@ -265,21 +321,45 @@ class ControlNode:
         self.publisher.publish(omniVel_msg)
 
     def update_target_position(self):
-        # Check if reached the current target position
-        if np.linalg.norm(self.pd_list[self.i] - self.p) < 0.1:
-            # Move to the next target position
-            self.i = (self.i + 1)
-            if self.i == len(self.pd_list):
-                self.i = len(self.pd_list)-1
+        if self.loadPose == True:
+            # Check if reached the current target position
+            #if np.linalg.norm(self.e_p) < 0.05:
+            elapsedTime = time.time() - self.prevTime
+            print(elapsedTime)
+            if  elapsedTime > 0.1:
+                self.prevTime = time.time()
+                # Move to the next target position
+                self.i = (self.i + 1)
+                #print('Posicion Alcanzada!')
+                # If reached the last target position
+                if self.i == len(self.pd_list):
+                    # Stay in position
+                    self.i = len(self.pd_list)-1
+                    
+    def save_pose_list(self):
+        rospack = rospkg.RosPack()
+        # get the file path for your_package
+        path = rospack.get_path('omni_control')
+        with open(path + '/pose_list_control.csv', 'w') as f:
+            writer = csv.writer(f)
+            for pose in self.pose_list:
+                writer.writerow(pose)
+        with open(path + '/pose_d_list_control.csv', 'w') as f:
+            writer = csv.writer(f)
+            for pose in self.posed_list:
+                writer.writerow(pose) 
+        print('Trajectory saved in:')
+        print(path)        
 
 if __name__ == '__main__':
     
     rospy.init_node('Control_SNPID', anonymous=True)
     control = ControlNode()
-    control.calibration()
     rate = rospy.Rate(10)  # Adjust the rate as needed (e.g., 10 Hz)
-    while not rospy.is_shutdown():
-        control.control() 
-        control.update_target_position()
-        rate.sleep()
-
+    try:
+        while not rospy.is_shutdown():
+            control.control() 
+            control.update_target_position()
+            rate.sleep()
+    except rospy.ROSInterruptException:
+        pass
--- scripts/omni_teleop.py
+++ scripts/omni_teleop.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
+import tf
+import csv
 import rospy
+import pickle
+import rospkg
 import numpy as np
 from tf.transformations import euler_from_quaternion, euler_from_matrix
 from geometry_msgs.msg import Quaternion, PoseStamped
@@ -10,6 +14,13 @@ class TeleOpNode:
         self.publisher = rospy.Publisher('omni_vel', Quaternion, queue_size=10)
         self.subscriberJoy  = rospy.Subscriber('joy', Joy, self.callback_joy)
         self.subscriberPose = rospy.Subscriber('posestamped', PoseStamped, self.callback_pose)
+        self.timer = rospy.Timer(rospy.Duration(0.1), self.timer_callback)
+        
+        self.firstCount = 0
+        self.firstPose = False
+        self.pose_list = []
+        self.quaternion_list = []
+        rospy.on_shutdown(self.save_pose_list)
         
         self.joyScaleX = 0.2
         self.joyScaleY = 0.2
@@ -21,7 +32,7 @@ class TeleOpNode:
         self.v_theta = 0
 
         self.p = np.array([0, 0, 0])
-        self.pp = np.array([0, 0, 0])
+        self.quaternion = np.array([0, 0, 0, 0])
         
     def callback_joy(self, data):
         
@@ -36,29 +47,25 @@ class TeleOpNode:
         self.publish_velocity()
 
     def callback_pose(self, data):
+    
+        # Get Pose from Message
         
-        # Simulated Pose
+        pose = (data.pose.position.x,
+                data.pose.position.y,
+                data.pose.position.z)
+        quaternion = (data.pose.orientation.x,
+                      data.pose.orientation.y,
+                      data.pose.orientation.z,
+                      data.pose.orientation.w)
+        
+        (roll,pitch,yaw) = euler_from_quaternion(quaternion)
+        self.p = np.array([data.pose.position.z,-data.pose.position.x,-pitch])
+        self.quaternion = np.array(quaternion)
 
-        q = (data.pose.orientation.x,
-             data.pose.orientation.y,
-             data.pose.orientation.z,
-             data.pose.orientation.w)
-        
-        (roll,pitch,yaw) = euler_from_quaternion(q)
-        print(roll,pitch,yaw)
-        # Transformation Matrix
-
-        gTc = self.transformer.fromTranslationRotation((data.pose.position.x,data.pose.position.y,data.pose.position.z),q)
-        cTo = np.array([[0,-1, 0, 1],
-                        [0, 0,-1, 1],
-                        [1, 0, 0, 1],
-                        [0, 0, 0, 1]]).astype(float)
-        gTo = np.dot(gTc,cTo)
-        gTo_rm = gTo[:3,:3]
-        (roll,pitch,yaw) = euler_from_matrix(gTo_rm,'sxyz')
-        
-        self.p = np.array([data.pose.position.x,data.pose.position.y,yaw])}
-        print(roll,pitch,yaw)
+        if self.firstPose == False:
+            self.firstPose = True
+            rospy.loginfo("Pose is being Published!!!")
+            rospy.loginfo("Waiting to Save the Pose...")
 
     def publish_velocity(self):
         
@@ -82,7 +89,7 @@ class TeleOpNode:
         
         self.u = np.dot(T,v)/0.05
         
-        #Publishing Wheel Velocities
+        # Publishing Wheel Velocities
 
         omniVel_msg = Quaternion()
         omniVel_msg.x = int(10*self.u[0])
@@ -91,12 +98,46 @@ class TeleOpNode:
         omniVel_msg.w = int(10*self.u[3])
 
         self.publisher.publish(omniVel_msg)
-
+        
+    def timer_callback(self, event):
+        
+        if self.firstPose == True:
+            if self.firstCount > 5:
+                self.pose_list.append(self.p)
+                self.quaternion_list.append(np.array(self.quaternion))
+                print("Pose Saved")
+            self.firstCount += 1
+        else:
+            rospy.loginfo("Waiting for Pose to be Published...")
+            
+    def save_pose_list(self):
+        rospack = rospkg.RosPack()
+        # get the file path for your_package
+        path = rospack.get_path('omni_control')
+        file_path = path + '/pose_list.csv'
+        with open(file_path, 'w') as f:
+            writer = csv.writer(f)
+            for pose in self.pose_list:
+                writer.writerow(pose)
+        print('Trajectory saved in:')
+        print(file_path)
+        
+	    # Save numpy array using pickle
+        pickle_file_path = path + '/np_array.pkl'
+        with open(pickle_file_path, 'wb') as f:
+            pickle.dump(self.quaternion_list, f)
+            print('Numpy array saved in:')
+            print(pickle_file_path)
+    
 if __name__ == '__main__':
     rospy.init_node('Omni_Teleop', anonymous=True)
     teleop = TeleOpNode()
 
     rate = rospy.Rate(10)
-    while not rospy.is_shutdown():
-        rate.sleep()
+    try:
+        while not rospy.is_shutdown():
+            rate.sleep()
+    except rospy.ROSInterruptException:
+        pass
+
 
